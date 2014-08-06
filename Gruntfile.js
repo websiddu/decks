53f25ee01e5356d865f93da47bf3762c50d8ccfd
Siddhartha Gudipati <me@websiddu.com>
2014-08-07 00:40:14 +0530

    Removing html min

--- Gruntfile.js
+++ Gruntfile.js
@@ -398,8 +398,8 @@ module.exports = function (grunt) {
         'copy:dist',
         'modernizr',
         //'rev',
-        'usemin',
-        'htmlmin'
+        'usemin'
+        //'htmlmin'
     ]);
 
     grunt.registerTask('default', [
